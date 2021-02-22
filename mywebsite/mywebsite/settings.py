a94fbb16d3d3c62c2210b6bf9ebc97fb716737b7
UtkarshW <uwankar8@gmail.com>
2021-02-22 20:07:22 +0530

    made changes again

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -23,7 +23,7 @@ from django.contrib import messages
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jfg!j0vu_%duwr6$8bp5oam4c3wempcnfu+!a&bn)bv*!mlyd8'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
